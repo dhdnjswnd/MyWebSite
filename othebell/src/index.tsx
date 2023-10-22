1373eab60d671ebf9309aa04e524abf568bebd6f
dhdnjswnd <dhdnjswnd@gmail.com>
2023-10-22 23:27:11 +0900

    #5 router추가

--- othebell/src/index.tsx
+++ othebell/src/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './HomePage';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import * as process from "process";
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router basename={process.env.PUBLIC_URL}>
+            <HomePage/>
+        </Router>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
