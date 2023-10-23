fb4091b77024d83e10be7b6caafc66ab84d5aa32
dhdnjswnd <dhdnjswnd@gmail.com>
2023-10-23 22:37:33 +0900

    #6 tab title 및 icon수정

--- othebell/src/index.tsx
+++ othebell/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,16 +6,35 @@ import HomePage from './HomePage';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import * as process from "process";
+import {createGlobalStyle} from 'styled-components';
+import { HelmetProvider, Helmet } from "react-helmet-async"; // 이부분 추가
 
 
+const TITLE = "OTHEBELL";
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --color-primary-theme: rgb(255, 212, 0);
+    --color-blue-theme: rgb(0, 121, 255);
+    --color-green-theme: rgb(100, 255, 100);
+  }`
+
+
 root.render(
     <React.StrictMode>
-        <Router basename={process.env.PUBLIC_URL}>
-            <HomePage/>
-        </Router>
+        <HelmetProvider>
+            <Helmet>
+                <title>{TITLE}</title>
+            </Helmet>
+            <GlobalStyles/>
+            <Router basename={process.env.PUBLIC_URL}>
+                <HomePage/>
+            </Router>
+        </HelmetProvider>
     </React.StrictMode>
 );
 
