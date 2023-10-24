44fd43c8256452111bc107dee3e53562c2911c11
dhdnjswnd <dhdnjswnd@gmail.com>
2023-10-24 17:51:13 +0900

    #6 툴팁 추가

--- othebell/src/Layout/Header.tsx
+++ othebell/src/Layout/Header.tsx
@@ -1,14 +1,40 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Image} from "react-bootstrap";
+import {Image, Nav} from "react-bootstrap";
 import soundcloudLogo from "../assets/img/soundcloud_img.png";
 import githubLogo from "../assets/img/github-mark.png";
 import tistoryLogo from "../assets/img/tistory_logo_black.png"
 import React from "react";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 
 function Header() {
+
+interface NavItemProps {
+  link: string;
+  src : string;
+  tooltipString:string;
+  className?:string;
+}
+    const NavItem : React.FC<NavItemProps> =
+({
+                         link,
+                         src,
+                         tooltipString,
+    className=""}) => (
+        <Nav.Item className={className}>
+            <Nav.Link href={link}>
+                <OverlayTrigger
+                    placement="bottom"
+                    delay={{show: 250, hide: 400}}
+                    overlay={<Tooltip>{tooltipString}</Tooltip>}
+                    defaultShow={true}>
+                    <Image src={src} rounded
+                           className="link_logo_image"/></OverlayTrigger>
+            </Nav.Link>
+        </Nav.Item>
+    );
     return (
         <>
             <Navbar className="HomeTitle">
@@ -18,18 +44,11 @@ function Header() {
                     <Navbar.Brand href="#home"><h2><b>OTHEBELL</b></h2></Navbar.Brand>
                 </Container>
                 <Container id="header_right_fixed" className="justify-content-end">
-                    <Nav.Item>
-                        <Nav.Link href="https://github.com/dhdnjswnd"><Image src={githubLogo} rounded
-                                                                             className="link_logo_image"/></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="https://soundcloud.com/9jmu75ppbekf"><Image src={soundcloudLogo} rounded
-                                                                                    className="link_logo_image"/></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className="pe-5">
-                        <Nav.Link href="https://developerpearl.tistory.com/"><Image src={tistoryLogo} rounded
-                                                                                    className="link_logo_image"/></Nav.Link>
-                    </Nav.Item>
+                    <NavItem link="https://github.com/dhdnjswnd" src={githubLogo} tooltipString="깃헙으로 바로가기"></NavItem>
+                    <NavItem link="https://soundcloud.com/9jmu75ppbekf" src={soundcloudLogo}
+                             tooltipString="사운드클라우드"></NavItem>
+                    <NavItem link="https://developerpearl.tistory.com/" src={tistoryLogo} tooltipString="티스토리 바로가기"
+                             className="pe-5"></NavItem>
                 </Container>
             </Navbar>
         </>
